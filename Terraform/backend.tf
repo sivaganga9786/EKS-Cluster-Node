3b89d7d9cbd04f3a28aec6668b1e65e529265539
siva <siva@example.com>
2025-08-17 14:16:32 +0530

    changed region

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraformec2346536567"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     key            = "eks/terraform.tfstate"
     workspace_key_prefix = "eks"
