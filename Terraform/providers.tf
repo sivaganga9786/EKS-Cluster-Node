042d143a268f2a62bdcb9ce8f16d0ee5fb640c52
siva <siva@example.com>
2025-08-17 18:38:20 +0530

    eks without helm

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -24,58 +24,21 @@ provider "aws" {
 #   }
 # }
 
-# Get EKS cluster details
-# data "aws_eks_cluster" "this" {
-#   name = module.eks.cluster_name
-# }
-
-# data "aws_eks_cluster_auth" "this" {
-#   name = module.eks.cluster_name
-# }
-
-# # Kubernetes provider
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.this.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.this.token
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.aws_eks_cluster.this.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-#     token                  = data.aws_eks_cluster_auth.this.token
-#   }
-# }
-
-
-
-###########################################################
-# DATA SOURCES FOR K8S PROVIDER
-###########################################################
-data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
-}
 
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
-###########################################################
-# KUBERNETES & HELM PROVIDERS
-###########################################################
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
-
-
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.eks.cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#     token                  = data.aws_eks_cluster_auth.this.token
+#   }
+# }
