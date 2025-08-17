c5b6a467ac640aed151224031138829592914ad2
siva <siva@example.com>
2025-08-17 17:14:45 +0530

    rerun eks

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -7,7 +7,7 @@ module "eks" {
   vpc_id                          = module.vpc.vpc_id
   subnet_ids                      = module.vpc.private_subnets
   control_plane_subnet_ids        = module.vpc.private_subnets
-  cluster_endpoint_public_access  = false
+  cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
   enable_irsa                     = true
 
