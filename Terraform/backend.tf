435aadfea2ad18fa6f499ccbba1417ca46090a78
siva <siva@example.com>
2025-08-17 14:22:11 +0530

    changed file name

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraformec2346536567"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    #dynamodb_table = "terraform-locks"
     key            = "eks/terraform.tfstate"
     workspace_key_prefix = "eks"
   }
