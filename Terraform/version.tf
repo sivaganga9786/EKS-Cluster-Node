deb46d92772da5f3fcd1529aa2d16bfb62c5b28d
siva <siva@example.com>
2025-08-17 14:08:59 +0530

    changed backed file

--- Terraform/version.tf
+++ Terraform/version.tf
@@ -5,11 +5,11 @@ terraform {
     helm        = { source = "hashicorp/helm", version = "~> 2.13" }
     kubernetes  = { source = "hashicorp/kubernetes", version = "~> 2.29" }
   }
-  backend "s3" {
-    bucket         = "ORG-tf-state"
-    key            = "eks/platform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "ORG-tf-locks"
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "ORG-tf-state"
+  #   key            = "eks/platform.tfstate"
+  #   region         = "ap-south-1"
+  #   dynamodb_table = "ORG-tf-locks"
+  #   encrypt        = true
+  # }
 }
